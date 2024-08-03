734090e913261bea1f6f33b635bf788f29c9301b
Tiago José Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2024-08-03 04:44:23 +0100

    opens window and run opengl loader

--- raftel/main.cpp
+++ raftel/main.cpp
@@ -4,6 +4,19 @@
 #include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello Wolrd" << std::endl;
+    glfwInit();
+
+    auto window = glfwCreateWindow(1280, 720, "Onto Raftael", nullptr, nullptr);
+    if(!window) return -1;
+
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
