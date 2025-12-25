c0d7318984d9fa29c9f741b406492ffbde9b6d32
Antypurus <tiagojsmagalhaes@gmail.com>
2025-12-25 03:58:15 +0000

    added call wrapper for windows funtions

--- raftel/Rendering/API/DX12/DX12.h
+++ raftel/Rendering/API/DX12/DX12.h
@@ -4,3 +4,35 @@
 #include <d3d12.h>
 #include <dxgi1_6.h>
 #include <wrl.h>
+
+#include <logger.h>
+
+namespace raftel {
+static std::pair<const char*, size_t> TranslateWindowErrorCode(HRESULT code)
+{
+    LPSTR message_buffer = nullptr;
+    size_t size = FormatMessageA(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        code,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPSTR)&message_buffer,
+        0,
+        NULL);
+    message_buffer[size - 1] = 0;
+    return { message_buffer, size };
+}
+}
+
+#define WIN_CALL(function_call, error_message, ...)                                                             \
+    {                                                                                                           \
+        HRESULT result##__LINE__ = function_call;                                                               \
+        if (result##__LINE__ != S_OK) {                                                                         \
+            auto error_message_##__LINE__ = raftel::TranslateWindowErrorCode(result##__LINE__);                 \
+            LOG_ERROR("{}", std::string_view(error_message_##__LINE__.first, error_message_##__LINE__.second)); \
+            LocalFree((LPSTR)error_message_##__LINE__.first);                                                   \
+            LOG_ERROR(error_message, __VA_ARGS__);                                                              \
+            __debugbreak();                                                                                     \
+            exit(-1);                                                                                           \
+        }                                                                                                       \
+    }
--- raftel/Rendering/API/DX12/DX12Renderer.cpp
+++ raftel/Rendering/API/DX12/DX12Renderer.cpp
@@ -7,31 +7,22 @@ namespace raftel::dx12 {
 
 DX12Renderer::DX12Renderer()
 {
-    HRESULT result = D3D12GetDebugInterface(IID_PPV_ARGS(&this->m_debug_controller));
-    if (result != S_OK) {
-        std::cerr << "Failed to load D3D12 debug controller" << std::endl;
-        exit(-1);
-    }
+    WIN_CALL(D3D12GetDebugInterface(IID_PPV_ARGS(&this->m_debug_controller)),
+        "Failed to load D3D12 Debug Controller");
     m_debug_controller->EnableDebugLayer();
     m_debug_controller->SetEnableGPUBasedValidation(TRUE);
     LOG_INFO("D3D12 Debug Controller Loaded & Activated");
 
-    result = CreateDXGIFactory2(DXGI_CREATE_FACTORY_DEBUG, IID_PPV_ARGS(&this->m_dxgi_factory));
-    if (result != S_OK) {
-        LOG_ERROR("Failed to create DXGI Factory");
-        exit(-1);
-    }
+    WIN_CALL(CreateDXGIFactory2(DXGI_CREATE_FACTORY_DEBUG, IID_PPV_ARGS(&this->m_dxgi_factory)),
+        "Failed to create DXGI Factory");
     LOG_INFO("DXGI Factory created");
 
     unsigned int adapter_index = 0;
     IDXGIAdapter1* adapter = nullptr;
     while (this->m_dxgi_factory->EnumAdapters1(adapter_index++, &adapter) == S_OK) {
         IDXGIAdapter4* interface_adapter = nullptr;
-        result = adapter->QueryInterface(IID_PPV_ARGS(&interface_adapter));
-        if (result != S_OK) {
-            std::cerr << "Failed to move factory interface from version 1 to version 4" << std::endl;
-            exit(-1);
-        }
+        WIN_CALL(adapter->QueryInterface(IID_PPV_ARGS(&interface_adapter)),
+            "Failed to move factory interface from version 1 to version 4");
 
         DXGI_ADAPTER_DESC3 adapter_desc;
         interface_adapter->GetDesc3(&adapter_desc);
