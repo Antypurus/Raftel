77517e3422be273a7647d947ec24e281b806a5c9
Antypurus <tiagojsmagalhaes@gmail.com>
2026-01-25 15:15:59 +0000

    added vulkan device selection code

--- raftel/Rendering/API/Vulkan/vulkan.cpp
+++ raftel/Rendering/API/Vulkan/vulkan.cpp
@@ -1,5 +1,6 @@
 #include <Rendering/API/Vulkan/vulkan.h>
 
+#include <cassert>
 #include <logger.h>
 #include <vector>
 #include <vulkan/vulkan.h>
@@ -47,6 +48,38 @@ void init_vulkan()
     };
     VK_CALL(vkCreateInstance(&instance_desc, nullptr, &instance), "Failed to create vulkan instance");
     LOG_SUCCESS("Vulkan Instance created");
+
+    uint32_t device_count = 0;
+    VK_CALL(vkEnumeratePhysicalDevices(instance, &device_count, nullptr), "Failed to obtain physical device count");
+
+    std::vector<VkPhysicalDevice> physical_devices(device_count);
+    VK_CALL(vkEnumeratePhysicalDevices(instance, &device_count, physical_devices.data()), "Failed to get physical devices");
+    assert(device_count > 0);
+
+    VkPhysicalDevice selected_device = nullptr;
+    if (device_count == 1) {
+        selected_device = physical_devices[0];
+    } else {
+        // find first discrete gpu
+        for (size_t i = 0; i < device_count; ++i) {
+            VkPhysicalDeviceProperties2 device_properties {
+                .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PROPERTIES_2,
+            };
+            vkGetPhysicalDeviceProperties2(physical_devices[i], &device_properties);
+            if (device_properties.properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
+                selected_device = physical_devices[i];
+                break;
+            }
+        }
+        // fallback onto first gpu in case a discrete gpu is not found
+        if (selected_device == nullptr) {
+            selected_device = physical_devices[0];
+        }
+    }
+
+    VkPhysicalDeviceProperties selected_device_properties;
+    vkGetPhysicalDeviceProperties(selected_device, &selected_device_properties);
+    LOG_INFO("Selected Device: {}", selected_device_properties.deviceName);
 }
 
 }
