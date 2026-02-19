f633b0b8e720ae76b7546cefba2c344a903a144f
Antypurus <tiagojsmagalhaes@gmail.com>
2026-02-19 20:29:47 +0000

    working device creation

--- raftel/Rendering/API/Vulkan/vulkan.cpp
+++ raftel/Rendering/API/Vulkan/vulkan.cpp
@@ -18,7 +18,6 @@ namespace raftel {
 void init_vulkan()
 {
     // ============== Instance Creation ======================
-    VkInstance instance = nullptr;
     const VkApplicationInfo app_desc = {
         .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
         .pNext = nullptr,
@@ -49,6 +48,7 @@ void init_vulkan()
         .enabledExtensionCount = (std::uint32_t)instance_extensions.size(),
         .ppEnabledExtensionNames = instance_extensions.data(),
     };
+    VkInstance instance = nullptr;
     VK_CALL(vkCreateInstance(&instance_desc, nullptr, &instance), "Failed to create vulkan instance");
     LOG_SUCCESS("Vulkan Instance created");
 
@@ -84,7 +84,9 @@ void init_vulkan()
     }
 
     VkPhysicalDeviceProperties selected_device_properties;
+    VkPhysicalDeviceFeatures selected_device_features;
     vkGetPhysicalDeviceProperties(selected_device, &selected_device_properties);
+    vkGetPhysicalDeviceFeatures(selected_device, &selected_device_features);
     LOG_INFO("Selected Device: {}", selected_device_properties.deviceName);
 
     // ================ Gather Queue Info =======================
@@ -93,31 +95,64 @@ void init_vulkan()
     std::vector<VkQueueFamilyProperties> queue_properties(queue_family_count);
     vkGetPhysicalDeviceQueueFamilyProperties(selected_device, &queue_family_count, queue_properties.data());
 
-    for (auto& queue_info : queue_properties) {
+    std::int32_t graphics_queue_index = -1;
+    std::int32_t compute_queue_index = -1;
+    std::int32_t transfer_queue_index = -1;
+    std::int32_t video_encode_queue_index = -1;
+    std::int32_t video_decode_queue_index = -1;
+    std::int32_t optical_flow_queue_index = -1;
+    std::vector<VkDeviceQueueCreateInfo> device_queue_info;
+    for (size_t i = 0; i < queue_properties.size(); ++i) {
+        auto& queue_info = queue_properties[i];
+        const float priorities[1] = { 0.0f };
+        device_queue_info.push_back({
+            .sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+            .pNext = nullptr,
+            .flags = 0,
+            .queueFamilyIndex = (std::uint32_t)i,
+            .queueCount = queue_info.queueCount,
+            .pQueuePriorities = priorities,
+        });
         if (queue_info.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
             LOG_INFO("Have {} Graphics Queues", queue_info.queueCount);
+            // graphics_queue_index = (std::int32_t)i;
         } else if (queue_info.queueFlags & VK_QUEUE_COMPUTE_BIT) {
             LOG_INFO("Have {} Compute Queues", queue_info.queueCount);
+            // compute_queue_index = (std::int32_t)i;
         } else if (queue_info.queueFlags & VK_QUEUE_VIDEO_DECODE_BIT_KHR) {
             LOG_INFO("Have {}, Video Decode Queues", queue_info.queueCount);
+            // video_decode_queue_index = (std::int32_t)i;
         } else if (queue_info.queueFlags & VK_QUEUE_VIDEO_ENCODE_BIT_KHR) {
             LOG_INFO("Have {}, Video Encode Queues", queue_info.queueCount);
+            // video_encode_queue_index = (std::int32_t)i;
         } else if (queue_info.queueFlags & VK_QUEUE_OPTICAL_FLOW_BIT_NV) {
             LOG_INFO("Have {}, Optical Flow Queues", queue_info.queueCount);
-        } else if (queue_info.queueFlags & VK_QUEUE_OPTICAL_FLOW_BIT_NV) {
-            LOG_INFO("Have {}, Data Graph Queues", queue_info.queueCount);
+            // optical_flow_queue_index = (std::int32_t)i;
         } else if (queue_info.queueFlags & VK_QUEUE_TRANSFER_BIT) {
             LOG_INFO("Have {}, Transfer Queues", queue_info.queueCount);
+            // transfer_queue_index = (std::int32_t)i;
         }
     }
 
     // ================= Device Creation =========================
-    /**
-    VkDeviceCreateInfo device_desc = {
+    const VkDeviceCreateInfo device_desc = {
         .sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
         .pNext = nullptr,
         .flags = 0,
-    };*/
+        .queueCreateInfoCount = (std::uint32_t)device_queue_info.size(),
+        .pQueueCreateInfos = device_queue_info.data(),
+        .enabledLayerCount = 0,
+        .ppEnabledLayerNames = nullptr,
+        .enabledExtensionCount = 0,
+        .ppEnabledExtensionNames = nullptr,
+        .pEnabledFeatures = &selected_device_features,
+    };
+    VkDevice logical_device = nullptr;
+    VK_CALL(vkCreateDevice(selected_device, &device_desc, nullptr, &logical_device), "Failed to create logical device");
+    LOG_SUCCESS("Vulkan Logical Device Created");
+
+    vkDestroyDevice(logical_device, nullptr);
+    vkDestroyInstance(instance, nullptr);
 }
 
 }
